12209d0857766861f4480031e4bddcbce701974e
lorilu <1229166932@qq.com>
2025-01-04 19:03:11 +0800

    Update process_changes.py
    修改信息描述

--- process_changes.py
+++ process_changes.py
@@ -193,8 +193,8 @@ def build_summary_file(title: str, url: str, summary: str, one_sentence: str) ->
 """
 
 def build_summary_readme_md(summarized_bookmarks: List[SummarizedBookmark]) -> str:
-    initial_prefix: str = """# Bookmark Summary 
-读取 bookmark-collection 中的书签，使用 jina reader 获取文本内容，然后使用 LLM 总结文本。详细实现请参见 process_changes.py。需要和 bookmark-collection 中的 Github Action 一起使用。
+    initial_prefix: str = """# bookmark-osmos-summary
+读取 bookmark-osmos 中的书签，使用 jina reader 获取文本内容，然后使用 LLM 总结文本。详细实现请参见 process_changes.py。需要和 bookmark-osmos 中的 Github Action 一起使用。
     
 ## Summarized Bookmarks
 """
